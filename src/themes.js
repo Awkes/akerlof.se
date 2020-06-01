6d1e81dfc65e570521c14442abd15af324750e88
Andreas Åkerlöf <a.akerlof@gmail.com>
2020-06-01 08:52:30 +0200

    feat(layout): base layout done

---
+++ src/themes.js
@@ -0,0 +1,28 @@
+export const themes = [
+  {
+    primary: '#009DCF',
+	  primaryTrans: 'rgba(0, 157, 207, 0.75)',
+  },
+  {
+    primary: '#0f0',
+	  primaryTrans: 'rgba(0, 255, 0, 0.75)',
+  },
+  {
+    primary: '#f0f',
+    primaryTrans: 'rgba(255, 0, 255, 0.75'
+  }
+]
+
+export function setTheme(index = null) {
+  if (!index) index = localStorage.getItem('theme') || 0;
+  else localStorage.setItem('theme', index);
+  
+  document.documentElement.style.setProperty(
+    "--primary",
+    themes[index].primary
+  );
+  document.documentElement.style.setProperty(
+    "--primary-trans",
+    themes[index].primaryTrans
+  );
+}
